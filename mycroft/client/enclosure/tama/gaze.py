fa980c5a8056fbed202f1226d9e9a2d338fffed6
dcm0 <donald.mcmillan@dsv.su.se>
2021-07-01 13:37:40 +0200

    Case -> if
    Boo Python!

--- mycroft/client/enclosure/tama/gaze.py
+++ mycroft/client/enclosure/tama/gaze.py
@@ -274,36 +274,35 @@ class EnclosureGaze:
         # 6 = playing output finshed
     def stateUpdate(self, event=None):
         if event:
-            match event.Message.msg_type:
-                case 'recognizer_loop:wakeword':
-                    resetVolume(self)
-                    updateLoop(self, 1)
-                    
-                case 'recognizer_loop:record_begin':
-                    updateLoop(self, 2)
+            if event.Message.msg_type == 'recognizer_loop:wakeword':
+                resetVolume(self)
+                updateLoop(self, 1)
                 
-                case 'recognizer_loop:record_end':
-                    updateLoop(self, 3)
+            if event.Message.msg_type == 'recognizer_loop:record_begin':
+                updateLoop(self, 2)
+            
+            if event.Message.msg_type == 'recognizer_loop:record_end':
+                updateLoop(self, 3)
+            
+            if event.Message.msg_type == 'recognizer_loop:utterance':
+                updateLoop(self, 4)
                 
-                case 'recognizer_loop:utterance':
-                    updateLoop(self, 4)
-                    
-                case 'recognizer_loop:speech.recognition.unknown':
-                    #recognition failed - what do?
-                    #send the shake command here, or catch this in eyes?
-                    updateLoop(self, 4)
+            if event.Message.msg_type == 'recognizer_loop:speech.recognition.unknown':
+                #recognition failed - what do?
+                #send the shake command here, or catch this in eyes?
+                updateLoop(self, 4)
+            
+            if event.Message.msg_type == 'recognizer_loop:audio_output_start':
+                updateLoop(self, 5)
                 
-                case 'recognizer_loop:audio_output_start':
-                    updateLoop(self, 5)
-                    
-                case 'recognizer_loop:audio_output_end':
-                    updateLoop(self, 6)
-                    #cameras update to 0 on this, keep local consistant even if it isn't used
-                    self.iloop = 0
-                    
-                    
-                case 'recognizer_loop:awoken':
-                    #This actually should be opening head as opposed to sleep closing it
+            if event.Message.msg_type == 'recognizer_loop:audio_output_end':
+                updateLoop(self, 6)
+                #cameras update to 0 on this, keep local consistant even if it isn't used
+                self.iloop = 0
                 
-                case 'recognizer_loop:DOA':
-                    updateDOA(self, event)
+                
+            if event.Message.msg_type == 'recognizer_loop:awoken':
+                #This actually should be opening head as opposed to sleep closing it
+            
+            if event.Message.msg_type == 'recognizer_loop:DOA':
+                updateDOA(self, event)
