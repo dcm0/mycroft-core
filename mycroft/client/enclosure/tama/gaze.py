b211372109edd8afd6e87ed6edcde34573bfe3cc
RazanJaber <razan.jabir@gmail.com>
2021-11-10 15:09:38 +0100

    degree30

--- mycroft/client/enclosure/tama/gaze.py
+++ mycroft/client/enclosure/tama/gaze.py
@@ -133,8 +133,9 @@ class CameraManager(Thread):
                         LOG.info("Face "+ self.camera_side + " p/y "+str(pitch)+" "+str(yaw)+" size "+str(face.size)+"  c:"+str(self.threadID))
                         
                         if(self.camera_side == 'R'):
+                            #sign does not affect the direction
                             x_sign = 0 
-                            x_m = -45
+                            x_m = -30
                             y_sign = 0
                             y_m = 0
                             LOG.info("In Move ")
