4284ea0ae8f4f79a91d4f130f7267334105c4af2
RazanJaber <razan.jabir@gmail.com>
2021-11-12 16:27:54 +0100

    move

--- mycroft/client/enclosure/tama/gaze.py
+++ mycroft/client/enclosure/tama/gaze.py
@@ -136,44 +136,17 @@ class CameraManager(Thread):
                         if(self.camera_side == 'R'):
                         
                             x_sign = 0 
-                            x_m = 0
-                            y_sign = 0
-                            y_m = 0
-                            LOG.info("In Move head ")
-                            update_pos='MOVE:'+str(x_sign)+":"+str(x_m)+":"+str(y_sign)+":"+str(y_m)+":\n"
-                            self.writer.write(update_pos) 
-
-                            time.sleep(3)
-
-                            #sign does not affect the direction, you can add the sign to the x_m, y_m
-                            x_sign = 0 
                             x_m = 30
                             y_sign = 0
                             y_m = 0
                             LOG.info("In Move head ")
                             update_pos='MOVE:'+str(x_sign)+":"+str(x_m)+":"+str(y_sign)+":"+str(y_m)+":\n"
                             self.writer.write(update_pos) 
-                            self.writer.write('PINK') 
 
-                            time.sleep(3)
+                            
 
-                            self.writer.write('GREEN')
+                            
 
-                            time.sleep(1.5)
-
-                            self.writer.write('PINK') 
-
-                            time.sleep(2)
-
-
-                            x_sign = 0 
-                            x_m = 0
-                            y_sign = 0
-                            y_m = 0
-                            LOG.info("In Move head ")
-                            update_pos='MOVE:'+str(x_sign)+":"+str(x_m)+":"+str(y_sign)+":"+str(y_m)+":\n"
-                            self.writer.write(update_pos) 
-                            self.writer.write('GREEN') 
                            
 
 
