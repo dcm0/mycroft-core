e2a3a5f84e10f7ccbdf594ee99eefaf7cbb092bf
RazanJaber <razan.jabir@gmail.com>
2021-11-12 17:40:45 +0100

    pos

--- mycroft/client/enclosure/tama/gaze.py
+++ mycroft/client/enclosure/tama/gaze.py
@@ -134,10 +134,10 @@ class CameraManager(Thread):
                         LOG.info("Face "+ self.camera_side + " p/y "+str(pitch)+" "+str(yaw)+" size "+str(face.size)+"  c:"+str(self.threadID))
                         
                         if(self.camera_side == 'R'):
-                            LOG.info("TEST " + str(face.pos_x)) 
-
+                            f_pos= int(-0.0379*(face.pos_x) + 19.092)
+                            LOG.info("TEST " + str(face.pos_x) + " " + str(f_pos)) 
                             x_sign = 0 
-                            x_m = -40
+                            x_m = 0
                             y_sign = 0
                             y_m = 0
                             LOG.info("In Move head ")
