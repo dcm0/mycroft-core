94d07a9d92cd251825debf33a3285d2ecb9ec4da
RazanJaber <razan.jabir@gmail.com>
2021-12-06 15:05:54 +0100

    tornedo

--- mycroft/client/enclosure/tama/flaskapp/app/views.py
+++ mycroft/client/enclosure/tama/flaskapp/app/views.py
@@ -1,8 +1,22 @@
-from flask import Flask, blueprints, redirect, url_for, render_template, request, Blueprint
+# views.py
+import json
+from tornado_sqlalchemy import SessionMixin
+from tornado.web import RequestHandler
 
-view = Blueprint('view',__name__)
+class BaseView(RequestHandler, SessionMixin):
+    """Base view for this application."""
 
-@view.route("/")
-def home():
-    return render_template('main.html')
-    
+    def prepare(self):
+        self.form_data = {
+            key: [val.decode('utf8') for val in val_list]
+            for key, val_list in self.request.arguments.items()
+        }
+
+    def set_default_headers(self):
+        """Set the default response header to be JSON."""
+        self.set_header("Content-Type", 'application/json; charset="utf-8"')
+
+    def send_response(self, data, status=200):
+        """Construct and send a JSON response with appropriate status code."""
+        self.set_status(status)
+        self.write(json.dumps(data))
