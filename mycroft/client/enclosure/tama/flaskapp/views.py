7b163a0ad21bcbcad6db3d6da4de22671bb5e128
RazanJaber <razan.jabir@gmail.com>
2021-12-06 11:51:07 +0100

    delete flask

--- mycroft/client/enclosure/tama/flaskapp/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, blueprints, redirect, url_for, render_template, request, Blueprint
-
-view = Blueprint('view',__name__)
-
-@view.route("/")
-def home():
-    return render_template('mainPage.html')
-    
-    
