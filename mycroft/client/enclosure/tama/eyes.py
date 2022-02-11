8ff88f18b29153c60cbd1bb4ad0de5ba6ffac4a5
Don <donald.mcmillan@dsv.su.se>
2022-02-11 15:03:59 +0100

    util audio play

--- mycroft/client/enclosure/tama/eyes.py
+++ mycroft/client/enclosure/tama/eyes.py
@@ -13,8 +13,9 @@
 # limitations under the License.
 
 from mycroft.util.log import LOG
+from mycroft.util import play_audio_file
 from mycroft.messagebus.message import Message
-from mycroft.skills.audioservice import AudioService
+
 
 class EnclosureEyes:
     """
@@ -28,7 +29,7 @@ class EnclosureEyes:
         self.writer = writer
         self.isOpen = False
         self.automove = True
-        self.audio_service = AudioService(self.bus)
+        
 
         self._num_pixels = 1 * 2
         self._current_rgb = [(255, 255, 255) for i in range(self._num_pixels)]
@@ -67,7 +68,7 @@ class EnclosureEyes:
 
     def playSound(self, soundString="bing"):
         LOG.info("BING Error sound ")
-        self.audio_service.play('file://./error.mp3')
+        play_audio_file('file://./error.mp3')
 
     def handle_get_color(self, message):
         """Get the eye RGB color for all pixels
