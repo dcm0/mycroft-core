f3c5db4cda448cae975f08767372465a7f7442f8
RazanJaber <razan.jabir@gmail.com>
2022-03-11 11:31:28 +0100

    fix

--- mycroft/client/enclosure/tama/eyes.py
+++ mycroft/client/enclosure/tama/eyes.py
@@ -263,7 +263,7 @@ class EnclosureEyes:
         x_sign = 0 
         x_m = event.data['pos']
         y_sign = 0
-        y_m = event.data['posVertical']
+        y_m = event.data['posvertical']
         update_pos='MOVE:'+str(x_sign)+":"+str(x_m)+":"+str(y_sign)+":"+str(y_m)+":\n"
         self.writer.write(update_pos)
 
