49db891970ecf8cce00e5a1fdfc7e8e5c009d6ae
RazanJaber <razan.jabir@gmail.com>
2022-07-15 11:05:51 +0200

    alarm sound added

--- mycroft/client/enclosure/tama/eyes.py
+++ mycroft/client/enclosure/tama/eyes.py
@@ -75,7 +75,7 @@ class EnclosureEyes:
 
 
     def playSound(self, message):
-        audiopath = str(Path().absolute()) +'/mycroft/client/enclosure/tama/error.mp3'
+        audiopath = str(Path().absolute()) +'/mycroft/client/enclosure/tama/alarm.mov'
         LOG.info("BING Error sound "+ audiopath)
         play_audio_file(audiopath)
 
